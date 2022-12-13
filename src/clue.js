50ba04978f5887400c755763a55f0c81720435dd
Dani Calvente <danicalvente@hotmail.com>
2022-12-13 13:57:48 +0100

    Solved Lab

--- src/clue.js
+++ src/clue.js
@@ -116,4 +116,6 @@ function pickMystery() {
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery(envelope) {
+  return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`;
+}
